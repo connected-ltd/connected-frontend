62e93356a442d973bf70046cb1ecf4d6759fdea3
Mubarak <mubee2004@gmail.com>
2024-10-04 17:02:09 +0100

    fixed missing module

--- src/custom-components/ResourcePreviewModal.tsx
+++ src/custom-components/ResourcePreviewModal.tsx
@@ -1,92 +1,92 @@
-import { Modal, Box, Typography } from "@mui/material";
-import { Resource } from "../types/resource.types";
+// import { Modal, Box, Typography } from "@mui/material";
+// import { Resource } from "../types/resource.types";
 
-type ResourceProps = {
-  open: boolean;
-  handleClose: () => void;
-  resource: Resource | null;
-};
+// type ResourceProps = {
+//   open: boolean;
+//   handleClose: () => void;
+//   resource: Resource | null;
+// };
 
-const ResourcePreviewModal = ({
-  open,
-  handleClose,
-  resource,
-}: ResourceProps) => {
-  const renderContent = () => {
-    if (!resource) return <Typography>No resource selected</Typography>;
+// const ResourcePreviewModal = ({
+//   open,
+//   handleClose,
+//   resource,
+// }: ResourceProps) => {
+//   const renderContent = () => {
+//     if (!resource) return <Typography>No resource selected</Typography>;
 
-    switch (resource.type) {
-      case "image":
-        return (
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <img
-              src={resource.url}
-              alt={resource.name}
-              style={{
-                maxWidth: "100%",
-                maxHeight: "80vh",
-              }}
-            />
-          </Box>
-        );
-      case "video":
-        return (
-          <video
-            src={resource.url}
-            controls
-            style={{ maxWidth: "100%", maxHeight: "80vh" }}
-          />
-        );
-      case "pdf":
-        return (
-          <>
-            <Typography
-              variant="h3"
-              sx={{ color: "#fff", textAlign: "center" }}
-            >
-              Loading
-            </Typography>
-            <iframe
-              src={resource.url}
-              title={resource.name}
-              width="100%"
-              height="80vh"
-            />
-          </>
-        );
-      default:
-        return <p>Unsupported file type</p>;
-    }
-  };
+//     switch (resource.type) {
+//       case "image":
+//         return (
+//           <Box
+//             sx={{
+//               display: "flex",
+//               justifyContent: "center",
+//               alignItems: "center",
+//             }}
+//           >
+//             <img
+//               src={resource.url}
+//               alt={resource.name}
+//               style={{
+//                 maxWidth: "100%",
+//                 maxHeight: "80vh",
+//               }}
+//             />
+//           </Box>
+//         );
+//       case "video":
+//         return (
+//           <video
+//             src={resource.url}
+//             controls
+//             style={{ maxWidth: "100%", maxHeight: "80vh" }}
+//           />
+//         );
+//       case "pdf":
+//         return (
+//           <>
+//             <Typography
+//               variant="h3"
+//               sx={{ color: "#fff", textAlign: "center" }}
+//             >
+//               Loading
+//             </Typography>
+//             <iframe
+//               src={resource.url}
+//               title={resource.name}
+//               width="100%"
+//               height="80vh"
+//             />
+//           </>
+//         );
+//       default:
+//         return <p>Unsupported file type</p>;
+//     }
+//   };
 
-  return (
-    <Modal open={open} onClose={handleClose}>
-      <Box
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          //   backgroundColor: "#fff",
-          //   padding: "2.5em",
-          //   borderRadius: "24px",
-          width: { xs: "90%", sm: "70%", md: "55vw" },
-          maxWidth: { xs: "75vw", md: "55vw" },
-        }}
-      >
-        {/* <IconButton onClick={handleClose}>
-          <Close />
-        </IconButton> */}
-        {renderContent()}
-      </Box>
-    </Modal>
-  );
-};
+//   return (
+//     <Modal open={open} onClose={handleClose}>
+//       <Box
+//         sx={{
+//           position: "absolute",
+//           top: "50%",
+//           left: "50%",
+//           transform: "translate(-50%, -50%)",
+//           //   backgroundColor: "#fff",
+//           //   padding: "2.5em",
+//           //   borderRadius: "24px",
+//           width: { xs: "90%", sm: "70%", md: "55vw" },
+//           maxWidth: { xs: "75vw", md: "55vw" },
+//         }}
+//       >
+//         {/* <IconButton onClick={handleClose}>
+//           <Close />
+//         </IconButton> */}
+//         {renderContent()}
+//       </Box>
+//     </Modal>
+//   );
+// };
 
-export default ResourcePreviewModal;
+// export default ResourcePreviewModal;
