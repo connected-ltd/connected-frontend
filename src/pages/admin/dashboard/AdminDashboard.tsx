d3a66f4fb634f98e97b97d6dde083ce92b777a99
Mubarak <mubee2004@gmail.com>
2024-10-16 17:58:46 +0100

    fetched number stats instead of fetching numbers and counting them from the backend

--- src/pages/admin/dashboard/AdminDashboard.tsx
+++ src/pages/admin/dashboard/AdminDashboard.tsx
@@ -1,26 +1,52 @@
 import { Box, LinearProgress, Typography } from "@mui/material";
 
-import { useGetAreasQuery, useGetNumbersQuery } from "../statsApiSlice";
+import { useGetAreasQuery, useGetNumbersStatsQuery } from "../statsApiSlice";
 import { DataSaverOff, Language, Map } from "@mui/icons-material";
 
 const AdminDashboard = () => {
   const { data: areas, isLoading: isFetchingAreas } = useGetAreasQuery();
-
-  const { data: numbers, isFetching: isFetchingNumbers } = useGetNumbersQuery();
+  const { data: numberStats, isLoading: isFetchingNumberStats } =
+    useGetNumbersStatsQuery();
   const totalAreas = areas?.data.length;
-  const totalNumbers = numbers?.data.length;
-  const hausaSepakers = numbers?.data.filter(
-    (number) => number.language === "hausa"
-  ).length;
-  const englishSpeakers = numbers?.data.filter(
-    (number) => number.language === "english"
-  ).length;
-  const igboSpeakers = numbers?.data.filter(
-    (number) => number.language === "igbo"
-  ).length;
-  const yorubaSpeakers = numbers?.data.filter(
-    (number) => number.language === "yoruba"
-  ).length;
+
+  // const overviewCard = [
+  //   {
+  //     id: 1,
+  //     icon: <Map />,
+  //     text: "Number of Locations",
+  //     number: totalAreas,
+  //   },
+  //   {
+  //     id: 2,
+  //     icon: <DataSaverOff />,
+  //     text: "Number of Numbers",
+  //     number: totalNumbers ? totalNumbers + 1128 : 0,
+  //   },
+  //   {
+  //     id: 3,
+  //     icon: <Language />,
+  //     text: "Number of English speakers",
+  //     number: englishSpeakers ? englishSpeakers + 775 : 0,
+  //   },
+  //   {
+  //     id: 4,
+  //     icon: <Language />,
+  //     text: "Number of Hausa speakers",
+  //     number: hausaSepakers ? hausaSepakers + 353 : 0,
+  //   },
+  //   {
+  //     id: 5,
+  //     icon: <Language />,
+  //     text: "Number of Igbo speakers",
+  //     number: igboSpeakers ? igboSpeakers + 0 : 0,
+  //   },
+  //   {
+  //     id: 6,
+  //     icon: <Language />,
+  //     text: "Number of Yoruba speakers",
+  //     number: yorubaSpeakers ? yorubaSpeakers + 0 : 0,
+  //   },
+  // ];
 
   const overviewCard = [
     {
@@ -32,51 +58,33 @@ const AdminDashboard = () => {
     {
       id: 2,
       icon: <DataSaverOff />,
-      text: "Number of Numbers",
-      number: totalNumbers ? totalNumbers + 1128 : 0,
+      text: "Total Numbers",
+      number: numberStats?.data[0]?.total,
     },
     {
       id: 3,
       icon: <Language />,
       text: "Number of English speakers",
-      number: englishSpeakers ? englishSpeakers + 775 : 0,
+      number: numberStats?.data[0]?.english,
     },
     {
-      id: 3,
+      id: 4,
       icon: <Language />,
       text: "Number of Hausa speakers",
-      number: hausaSepakers ? hausaSepakers + 353 : 0,
+      number: numberStats?.data[0]?.hausa,
     },
     {
-      id: 3,
+      id: 5,
       icon: <Language />,
       text: "Number of Igbo speakers",
-      number: igboSpeakers ? igboSpeakers + 0 : 0,
+      number: numberStats?.data[0]?.igbo,
     },
     {
-      id: 3,
+      id: 6,
       icon: <Language />,
       text: "Number of Yoruba speakers",
-      number: yorubaSpeakers ? yorubaSpeakers + 0 : 0,
+      number: numberStats?.data[0]?.yoruba,
     },
-    // {
-    //   id: 4,
-    //   icon: notesIcon,
-    //   text: "Class Notes",
-    //   number: "99",
-    // },
-    // {
-    //   id: 5,
-    //   icon: mediaIcon,
-    //   text: "Media Library",
-    //   number: "12",
-    // },
-    // {
-    //   id: 6,
-    //   icon: reviewIcon,
-    //   text: "Reviews",
-    //   number: "0",
-    // },
   ];
 
   return (
@@ -93,7 +101,7 @@ const AdminDashboard = () => {
           gap: { xs: 2.5, sm: 2, md: 2, lg: 2.5 },
         }}
       >
-        {isFetchingAreas || (isFetchingNumbers && <LinearProgress />)}
+        {(isFetchingAreas || isFetchingNumberStats) && <LinearProgress />}
         {overviewCard.map((card) => (
           <Box
             sx={{
